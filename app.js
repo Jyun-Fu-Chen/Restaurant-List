1514f02646b798b21532fd9677851ea484accee4
Jyun-Fu-Chen <than3698741@gmail.com>
2022-02-16 13:06:49 +0800

    更改搜尋功能從main換到index show頁面完成

--- app.js
+++ app.js
@@ -14,9 +14,14 @@ app.get('/',(req, res)=>{
   const restaurant = restaurantList.results
   res.render('index',{restaurants: restaurant})
 })
+app.get('/restaurants/:restaurant_id',(req,res)=>{
+  console.log(req.params.restaurant_id)
+  const restaurantFiltered = restaurantList.results.filter((restaurant)=>restaurant.id === Number(req.params.restaurant_id))
+  console.log(restaurantFiltered)
+  res.render('show', {restaurants: restaurantFiltered[0]})
 
-
+})
 
 app.listen(port,()=>{
-  console.log('express is listening')
+  console.log(`express is listening localhost:${port}`)
 })
