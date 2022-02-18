f9b60fd9c114a828a8d5afb03de33b9a36078730
Jyun-Fu-Chen <than3698741@gmail.com>
2022-02-19 01:34:24 +0800

    feat: add Create,Edit function

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose')
 const db = mongoose.connection
-const restaurantList = require('./restaurant.json')
+const bodyParser = require('body-parser')
 
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
+app.use(express.urlencoded({extended: true}))
 
 mongoose.connect('mongodb://localhost/restaurant-list')
 db.on('error', ()=>{
@@ -35,23 +36,73 @@ const id = req.params.restaurant_id
   .lean()
     .then(restaurant => res.render('show', {restaurant}))
     .catch(error => console.log(error))
-  // const restaurantFiltered = restaurantList.results.filter((restaurant)=>restaurant.id === Number(req.params.restaurant_id))
-  // res.render('show', {restaurants: restaurantFiltered[0]})
 })
 
 //搜尋功能
 app.get('/search',(req,res)=>{
-  const keyword = req.query.keyword.toLowerCase()
-  const restaurant = restaurantList.results.filter(function (item){
-    if (item.name.toLowerCase().includes(keyword) || item.category.toLowerCase().includes(keyword)){
-    return item
+  const keyword = req.query.keywords.toLowerCase()
+   if(keyword.length === 0 ){
+  return res.redirect("/")
   }
+  
+  Restaurant.find()
+  .lean()
+  .then(restaurant => {
+    const filterRestaurant = restaurant.filter(data => data.name.toLowerCase().includes(keyword) || data.category.toLowerCase().includes(keyword)
+    )
+    res.render("index",{restaurant: filterRestaurant})
+  })
+    .catch(error => console.log(error))
 })
-  res.render('index', { restaurants: restaurant })
+
+//新增餐廳頁面
+app.get("/go_new_page",(req,res)=>{
+  return res.render('new')
+})
+//新增餐廳功能
+app.post("/new_restaurant",(req,res)=>{
+const new_restaurant = req.body.name
+Restaurant.create(req.body)
+.then(() => res.redirect("/"))
+.catch(error => console.log(error))
+})
+
+
+
+//編輯頁面
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
 })
 //編輯功能
+app.post('/restaurants/:restaurant_id/edit',(req, res) => {
+  const id = req.params.restaurant_id
+  const data = req.body
+  return Restaurant.findById(id)
+ .then(restaurant =>{
+   restaurant.name = data.name
+   restaurant.name_en = data.name_en
+   restaurant.category = data.category
+   restaurant.image = data.image
+   restaurant.location = data.location
+   restaurant.phone = data.phone
+   restaurant.google_map = data.google_map
+   restaurant.rating = data.rating
+   restaurant.description = data.description
+   console.log(data.category)
+   return restaurant.save()
+ })
+  .then(() => {
+    res.redirect(`/restaurants/${id}`)
+  })
+  .catch(error=> console.log(error))
+})
 
-//新增餐廳功能
+
+
 
 // 刪除功能
 
