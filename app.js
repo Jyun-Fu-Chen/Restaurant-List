2acdb3713ed0bdc1e4a3eeac1f8ef617034aa8ec
Jyun-Fu-Chen <than3698741@gmail.com>
2022-02-18 14:21:30 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+const db = mongoose.connection
 const restaurantList = require('./restaurant.json')
 
 const exphbs = require('express-handlebars')
 const req = require('express/lib/request')
-
+mongoose.connect('mongodb://localhost/restaurant-list')
+db.on('error', ()=>{
+  console.log('mongodb error!')
+})
+db.once('open',()=>{
+  console.log('mongodb connected!')
+})
 
 app.engine('handlebars',exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
