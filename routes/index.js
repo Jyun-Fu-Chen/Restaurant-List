311d9d9fafbe5a63b9ac792b6f4aea62aece63f1
Jyun-Fu-Chen <than3698741@gmail.com>
2022-03-21 17:45:42 +0800

    feat: add users route and login,register handlebars

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurant = require('./modules/restaurants')
+const user = require('./modules/users')
 
-router.use('/',home)
-router.use('/restaurants',restaurant)
+router.use('/', home)
+router.use('/restaurants', restaurant)
+router.use('/users', user)
 
 module.exports = router
