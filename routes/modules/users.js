8213653f4fd0911d0f1253cbec62f71f48d964a7
Jyun-Fu-Chen <than3698741@gmail.com>
2022-03-23 10:38:10 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 
 //登入頁面
@@ -43,11 +44,14 @@ router.post('/register', (req, res) => {
           confirmPassword
         })
       } else {
-        return User.create({
-          name,
-          email,
-          password
-        })
+        return bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(password, salt))
+          .then(hash => User.create({
+            name,
+            email,
+            password: hash
+          }))
           .then(() => res.redirect('/'))
           .catch(err => console.log(err))
       }
